7197f9c930616eca87f76c2e0e260f4a1174c4e6
GDUT-dmt-30 <1535277375@qq.com>
2019-03-26 17:12:05 +0800

    3.26 update

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-define('MYSQL_HOST','39.108.237.9');
-define('MYSQL_USER','fenglei');
-define('MYSQL_PW','Feng123456!');
-define('MYSQL_DATABASE','test_fl');
+define('MYSQL_HOST','134.175.59.86');
+define('MYSQL_USER','dmt');
+define('MYSQL_PW','Dmt123456!');
+define('MYSQL_DATABASE','dmt');
